e24731c11c01be1b350adad31a23fae371389cc2
Yasmin Arnaut <yasminarnaut87@gmail.com>
2022-05-09 19:50:17 -0300

    Commit Inicial

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,25 @@
+import { TypesComponent } from './../types/types.component';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pokemon } from './../types/pokemon';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  pokemons: Pokemon[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.carregarPokemons();
+  }
+
+  async carregarPokemons() {
+    const requisicao = await this.httpClient
+      .get<any>('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .toPromise();
+
+    this.pokemons = requisicao.results;
+    console.log(this.pokemons)
+
+  }
+}
